b13d764eb95db8985dc731bef7fcff4b94eb87ed
antonis.liakakis <antonis.liakakis@marinetraffic.com>
2018-01-10 16:46:58 +0200

    Moved index.js to root folder

---
+++ index.js
@@ -0,0 +1,11 @@
+import Conditional from './src/conditional.component';
+import If from './src/if.component';
+import Else from './src/else.component';
+import Repeat from './src/repeat.component';
+
+export {
+    Conditional,
+    If,
+    Else,
+    Repeat,
+}
